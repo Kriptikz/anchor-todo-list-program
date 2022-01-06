233ba8d42d33ac2f2cf0caaf7f401a37f35b59f0
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-06 21:02:05 +0100

    Created account data storage structs

--- programs/todo-list/src/lib.rs
+++ programs/todo-list/src/lib.rs
@@ -13,7 +13,17 @@ pub mod todo_list {
 #[derive(Accounts)]
 pub struct InitializeList {}
 
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Task {
+    pub name: String,
+    pub completed: bool,
+    pub verified: bool,
+    pub amount: u64,
+}
+
 #[account]
 pub struct TodoList {
-
+    pub authority: Pubkey,
+    pub tasks: Vec<Task>,
+    pub vault: Pubkey,
 }
