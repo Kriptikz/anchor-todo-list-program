dcae2e975b4c4f690a2c985231a9a05557cc1164
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-06 19:24:04 +0100

    Renamed initialize function to initialize_list Renamed Initialize Context to InitializeList

--- programs/todo-list/src/lib.rs
+++ programs/todo-list/src/lib.rs
@@ -5,10 +5,15 @@ declare_id!("57p2dLPetHZKhBw78PbMPwiW1fLwfEMziA8xLd3r5kvm");
 #[program]
 pub mod todo_list {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize_list(ctx: Context<InitializeList>) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeList {}
+
+#[account]
+pub struct TodoList {
+
+}
